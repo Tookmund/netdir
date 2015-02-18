f48932287d11d90f51a351d7346c002d93e62f0b
Jacob Adams <tookmund@gmail.com>
2015-02-18 12:39:17 -0500

    Work on Netserver

--- netserver.c
+++ netserver.c
@@ -13,41 +13,50 @@ int main (int argc, char* argv[]) {
 	}
 
 	ret = mkfifo("create",0666);
-	if (ret < 0) perror("Could not make create fifo: ");
-
-	int ctfd = open("create",O_RDONLY);
-	if (ctfd < 0) perror("Could not open create: ");
-
-	char create;
-	char* cte = &create;
-	
+	if (ret < 0) {
+		perror("Could not make create fifo");
+		return 0;
+	}
+	int ctfd = open("create",O_RDWR);
+	if (ctfd < 0) {
+		perror("Could not open create");
+		return 0;
+	}
+	char create[1];
 	//socket counter
 	int count = 0;
+	char crdir[15];
 
-	for(;;ret = read(ctfd,cte,1)) {
+	for(;;ret = read(ctfd,create,1)) {
+		//printf("%s %d\n",create,count);
 		if (ret < 0) {
-			perror("Failed to read from create: ");
+			perror("Failed to read from create");
 			continue;
 		}
-		char crdir[ ] = { (char)count,'\0' };
+		else if (ret < 1 || create[0] == '\n') {
+			continue;
+		}
+
+		sprintf(crdir,"%d",count);
 		ret = mkdir(crdir,0777);
 		if (ret < 0) {
-			printf("Failed to create dir ");
+			printf("Failed to create dir");
 			perror("");
 			continue;
 		}
 		count++;
-		printf("%d",count);
 		switch(fork()) {
-		case -1:
-			perror("Failed to fork: ");
-			break;
-		case 0:
-			break;
-		default:
-			//char* argv[ ] = {"netclient",cte};
-			exit(0);
+			case -1:
+				perror("Failed to fork");
+				break;
+			case 0:
+				printf("%d\n",count);
+				break;
+			default:
+				//char* argv[ ] = {"netclient",cte};
+				exit(0);
 		}
+		printf("test");
 	}
 	//execvp()
 	return 0;
